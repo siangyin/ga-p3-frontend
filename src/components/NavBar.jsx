a9efacd1b8b6288066bf1ab9210bb9a21211de3c
Lee Siang Yin <siangyin916@gmail.com>
2021-12-18 15:34:35 +0800

    💿npm i react-icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 // import { useState, useEffect, useRef } from "react";
 // import { a } from "react-router-dom";
-
+import { FaSearch, FaHeart } from "react-icons/fa";
 
 export default function NavBar() {
 	return (
@@ -38,36 +38,12 @@ export default function NavBar() {
 			</div>
 			<div className="flex-none">
 				<button className="btn btn-square btn-ghost">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						className="inline-block w-6 h-6 stroke-current"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-						></path>
-					</svg>
+					<FaHeart />
 				</button>
 			</div>
 			<div className="flex-none">
 				<button className="btn btn-square btn-ghost">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						className="inline-block w-6 h-6 stroke-current"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-						></path>
-					</svg>
+					<FaSearch />
 				</button>
 			</div>
 		</div>
