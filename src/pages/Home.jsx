8d44e00f01820be42edd0ce7c1d9cdd263f64a8d
Lee Siang Yin <siangyin916@gmail.com>
2021-12-18 14:25:41 +0800

    🌸 homepage style is working - draft version ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
 		<div className="hero min-h-screen bg-base-200">
 			<div className="flex-col justify-center hero-content lg:flex-row">
 				<div className="text-center lg:text-left">
-					<h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+					<h1 className="mb-5 text-5xl font-bold">Helloooooo</h1>
 					<p className="mb-5">
 						Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
 						excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
@@ -16,11 +16,11 @@ const Home = () => {
 					<div className="card-body">
 						<div className="form-control">
 							<label className="label">
-								<span className="label-text">Email</span>
+								<span className="label-text">Email/Username</span>
 							</label>
 							<input
 								type="text"
-								placeholder="email"
+								placeholder="Email/Username"
 								className="input input-bordered"
 							></input>
 						</div>
@@ -32,18 +32,24 @@ const Home = () => {
 								type="text"
 								placeholder="password"
 								className="input input-bordered"
-							>
-								<label className="label">
-									<a href="https://www.google.com/" className="label-text-alt">
-										Forgot password?
-									</a>
-								</label>
-							</input>
+							></input>
+							<label className="label">
+								<a href="#" className="label-text-alt">
+									Forgot password?
+								</a>
+							</label>
 						</div>
-						<div className="form-control mt-6">
+						<div className="form-control mt-3">
 							<input
 								type="button"
 								value="Login"
+								className="btn btn-primary"
+							></input>
+						</div>
+						<div className="form-control mt-3">
+							<input
+								type="button"
+								value="Register"
 								className="btn btn-primary"
 							></input>
 						</div>
