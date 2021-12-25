f60e1339e12cae5c400a41eaaea2c206f023635f
Wei Lun <garyganweilun@gmail.com>
2021-12-25 22:11:21 +0800

    Added login and registeration

--- src/components/AccountForm.jsx
+++ src/components/AccountForm.jsx
@@ -1,39 +1,67 @@
-export default function AccountForm()
-{
-    return(
-        
-			<div className="card flex-shrink-0 w-full max-w-sm justify-center shadow-2xl bg-base-100 md:w-1/2">
-            <div className="card-body">
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Email/Username</span>
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="Email/Username"
-                        className="input input-bordered"
-                    ></input>
-                </div>
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text">Password</span>
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="password"
-                        className="input input-bordered"
-                    ></input>
-                    <label className="label">
-                        <a href="#testing" className="label-text-alt">
-                            Forgot password?
-                        </a>
-                    </label>
-                </div>
+import axios from "axios";
+import React from "react";
+import { useForm } from "react-hook-form"
 
-                <div className="card-actions">
-                    <button className="btn  hover:btn-primary">Login</button>
-                    <button className="btn  hover:btn-primary">Register</button>
-                </div>
+export default function AccountForm() {
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
+    const onSubmitLogin = (data) => {
+        axios({
+            method: 'post',
+            url: "https://kitchentracker.herokuapp.com/login",
+            data: data
+        }).then(res => console.log(res.data)).catch(err => console.log(err))
+    }
+
+    const onRegisterLogin = (data) => {
+        axios({
+            method: 'post',
+            url: "https://kitchentracker.herokuapp.com/signup",
+            data: data
+        }).then(res => console.log(res.data)).catch(err => console.log(err))
+    }
+
+
+
+    return (
+
+        <div className="card flex-shrink-0 w-full max-w-sm justify-center shadow-2xl bg-base-100 md:w-1/2">
+            <div className="card-body">
+                <form onSubmit={e => e.preventDefault()}>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Username</span>
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            className="input input-bordered"
+                            {...register('username', { required: true })}
+                        />
+                        {errors.username && <p className="text-pink text-xs italic">This field is required</p>}
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="password"
+                            className="input input-bordered"
+                            {...register("password", { required: true })}
+                        />
+                        {errors.password && <p className="text-pink text-xs italic">This field is required</p>}
+                        <label className="label">
+                            <a href="#testing" className="label-text-alt">
+                                Forgot password?
+                            </a>
+                        </label>
+                    </div>
+                    <div className="card-actions">
+                        <button className="btn  hover:btn-primary" onClick={handleSubmit(onSubmitLogin)}>Login</button>
+                        <button className="btn  hover:btn-primary" onClick={handleSubmit(onRegisterLogin)}>Register</button>
+                    </div>
+                </form>
             </div>
         </div>
     )
