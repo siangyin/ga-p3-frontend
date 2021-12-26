dab60b6d02fdc0d2297509eb409cb77747e8418c
Wei Lun <garyganweilun@gmail.com>
2021-12-26 20:39:33 +0800

    Change back URL and navigate after authenciation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Route, Routes, Navigate} from "react-router-dom"
+import {Route, Routes} from "react-router-dom"
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
