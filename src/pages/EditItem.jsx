c64d65e4c1d7e604fd30847d28c60a571e77a8eb
Wei Lun <garyganweilun@gmail.com>
2022-01-10 01:28:53 +0800

    Added edit items

--- src/pages/EditItem.jsx
+++ src/pages/EditItem.jsx
@@ -24,7 +24,7 @@ const EditItem = () => {
         await axios.get(`http://localhost:5000/api/v1/items/${location.state.itemsID}`, { withCredentials: true })
             .then(res => {
                 setItemDetails(res.data.data)
-                reset({keepDirty: true})
+                reset({ keepDirty: true })
             })
     }
 
@@ -142,85 +142,93 @@ const EditItem = () => {
                         Edit Item
                     </h1>
                 </div>
-                <form className="form-control flex flex-wrap mt-6" onSubmit={e => e.preventDefault()}>
-                    <input
-                        type="text"
-                        defaultValue={getItemDetails.name}
-                        className="input input-bordered mb-3  sm:w-1/2 w-full place-self-center"
-                        {...register('name', { required: true, })}
+                <div className="grid place-items-center">
+                    <img
+                        src={getItemDetails.imgUrl}
+                        alt="item"
+                        className="object-center object-cover"
+                        width= '500'
+                    ></img>
+                </div>
+            <form className="form-control flex flex-wrap mt-6" onSubmit={e => e.preventDefault()}>
+                <input
+                    type="text"
+                    defaultValue={getItemDetails.name}
+                    className="input input-bordered mb-3  sm:w-1/2 w-full place-self-center"
+                    {...register('name', { required: true, })}
 
-                    ></input>
+                ></input>
 
-                    <input
-                        type="text"
-                        defaultValue={getItemDetails.brand}
-                        className="input input-bordered mb-3  sm:w-1/2 w-full place-self-center"
-                        {...register('brand', { required: true })}
-                    ></input>
+                <input
+                    type="text"
+                    defaultValue={getItemDetails.brand}
+                    className="input input-bordered mb-3  sm:w-1/2 w-full place-self-center"
+                    {...register('brand', { required: true })}
+                ></input>
 
-                    <input
-                        type="date"
-                        defaultValue={expiryDate}
-                        className="input input-bordered mb-3  sm:w-1/2 w-full  place-self-center"
-                        {...register('expiryDate', { required: true })}
-                    ></input>
+                <input
+                    type="date"
+                    defaultValue={expiryDate}
+                    className="input input-bordered mb-3  sm:w-1/2 w-full  place-self-center"
+                    {...register('expiryDate', { required: true })}
+                ></input>
 
-                    <input
-                        type="number"
-                        id="quantity"
-                        defaultValue={getItemDetails.qty}
-                        min="0"
-                        className="input input-bordered mb-3  sm:w-1/2 w-full place-self-center"
-                        {...register('qty')}
-                    ></input>
+                <input
+                    type="number"
+                    id="quantity"
+                    defaultValue={getItemDetails.qty}
+                    min="0"
+                    className="input input-bordered mb-3  sm:w-1/2 w-full place-self-center"
+                    {...register('qty')}
+                ></input>
 
-                    <select
-                        className="select select-bordered mb-3 sm:w-1/2 w-full place-self-center"
-                        name="grpName" value={selectedCollection} onChange={handleChange} defaultValue="Collection"
-                    >
-                        <option disabled="disabled" value="Collection">
-                            Select Collection
-                        </option>
-                        {options}
-                    </select>
+                <select
+                    className="select select-bordered mb-3 sm:w-1/2 w-full place-self-center"
+                    name="grpName" value={selectedCollection} onChange={handleChange} defaultValue="Collection"
+                >
+                    <option disabled="disabled" value="Collection">
+                        Select Collection
+                    </option>
+                    {options}
+                </select>
 
-                    <input
-                        type="text"
-                        placeholder="Product Image URL"
-                        defaultValue={getItemDetails.imgUrl}
-                        className="input input-bordered mb-3  sm:w-1/2 w-full place-self-center"
-                        {...register('imgUrl')}
-                    ></input>
+                <input
+                    type="text"
+                    placeholder="Product Image URL"
+                    defaultValue={getItemDetails.imgUrl}
+                    className="input input-bordered mb-3  sm:w-1/2 w-full place-self-center"
+                    {...register('imgUrl')}
+                ></input>
 
-                    <div className="p-6 place-self-center">
-                        <div className="form-control">
-                            <label className="cursor-pointer label">
-                                <span className="label-text mr-5">Add to Favourite List</span>
-                                <input
-                                    type="checkbox"
-                                    className="toggle"
-                                    defaultChecked={getItemDetails.fav}
-                                    {...register('fav')}
-                                ></input>
-                            </label>
-                        </div>
+                <div className="p-6 place-self-center">
+                    <div className="form-control">
+                        <label className="cursor-pointer label">
+                            <span className="label-text mr-5">Add to Favourite List</span>
+                            <input
+                                type="checkbox"
+                                className="toggle"
+                                defaultChecked={getItemDetails.fav}
+                                {...register('fav')}
+                            ></input>
+                        </label>
                     </div>
+                </div>
 
-                    <div className="flex flex-row place-self-center space-x-4">
-                        <button className="btn btn-outline btn-primary" onClick={handleSubmit(onCreate)}>
-                            Save & go to List
-                        </button>
+                <div className="flex flex-row place-self-center space-x-4">
+                    <button className="btn btn-outline btn-primary" onClick={handleSubmit(onCreate)}>
+                        Save & go to List
+                    </button>
 
-                        <button className="btn btn-outline btn-primary" onClick={handleSubmit(onAddAnother)}>
-                            Save & Add another
-                        </button>
+                    <button className="btn btn-outline btn-primary" onClick={handleSubmit(onAddAnother)}>
+                        Save & Add another
+                    </button>
 
-                        <button className="btn btn-outline btn-primary" onClick={handleSubmit(onClear)}>
-                            Clear/ Delete
-                        </button>
-                    </div>
-                </form>
-            </div>
+                    <button className="btn btn-outline btn-primary" onClick={handleSubmit(onClear)}>
+                        Clear/ Delete
+                    </button>
+                </div>
+            </form>
+        </div>
         </>
     );
 };
