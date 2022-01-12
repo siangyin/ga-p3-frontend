dede1a5905a225f3c34b6dac9234037d96c67b05
Lee Siang Yin <siangyin916@gmail.com>
2022-01-12 19:58:29 +0800

    ✅ toggle menu bar for mobile view

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,10 +6,13 @@ import { AuthContext } from "../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { useState } from "react";
 
 export default function NavBar() {
-	const userSession = useContext(AuthContext)
-	let navigate = useNavigate()
+	const userSession = useContext(AuthContext);
+	let navigate = useNavigate();
+	const [showLinks, setShowLinks] = useState(true);
+	console.log(showLinks);
 	const onLogout = async () => {
 		try {
 			await axios
@@ -36,22 +39,25 @@ export default function NavBar() {
 		}
 	};
 
-	return (
-		<>
-			{userSession ? (
-				<nav className="flex flex-wrap items-center justify-between bg-base-200 p-5 shadow-md">
-					<div className="flex items-center flex-shrink-0 mr-8">
-						<Link to="/">
-							<img src={logo} alt="logo" width="200px"></img>
-						</Link>
-					</div>
+	if (userSession) {
+		return (
+			<nav className="flex flex-wrap items-center justify-between p-6">
+				<div className="flex items-center flex-shrink-0 mr-8">
+					<Link to="/">
+						<img src={logo} alt="logo" width="200px"></img>
+					</Link>
+				</div>
 
-					<div className="block sm:hidden">
-						<button className="flex items-center px-3 py-2 border rounded border-grey-400 hover:text-primary">
-							<FaBars />
-						</button>
-					</div>
+				<div className="block sm:hidden">
+					<button
+						className="flex items-center px-3 py-2 border rounded border-grey-400 hover:text-primary"
+						onClick={() => setShowLinks(!showLinks)}
+					>
+						<FaBars />
+					</button>
+				</div>
 
+				{showLinks && (
 					<div className="w-full block flex-grow sm:flex sm:items-center sm:w-auto justify-end">
 						<button className="block sm:inline-block sm:mt-0 text-2xl  hover:text-primary mr-5">
 							<Link to="items">Items</Link>
@@ -69,22 +75,17 @@ export default function NavBar() {
 							Logout
 						</button>
 					</div>
-				</nav>
-			) : (
-				<nav className="flex flex-wrap items-center justify-between p-6">
-					<div className="flex items-center flex-shrink-0 mr-8">
-						<Link to="/">
-							<img src={logo} alt="logo" width="200px"></img>
-						</Link>
-					</div>
-
-					<div className="block sm:hidden">
-						<button className="flex items-center px-3 py-2 border rounded border-grey-400 hover:text-primary">
-							<FaBars />
-						</button>
-					</div>
-				</nav>
-			)}
-		</>
+				)}
+			</nav>
+		);
+	}
+	return (
+		<nav className="flex flex-wrap items-center justify-between p-6">
+			<div className="flex items-center flex-shrink-0 mr-8">
+				<Link to="/">
+					<img src={logo} alt="logo" width="200px"></img>
+				</Link>
+			</div>
+		</nav>
 	);
 }
